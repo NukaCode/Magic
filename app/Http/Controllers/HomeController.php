970facebc6b25e151088578c38d8d9581f1f4819
stygiansabyss <stygian.warlock.v2@gmail.com>
2015-07-19 22:35:04 -0500

    Layout work.  Adding CI configs.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends Controller
 
     public function index(Card $card)
     {
-        $cards = $card->all();
+        $cards = $card->where('type', '!=', 'Land')->get();
 
         $this->setJavascriptData(compact('cards'));
     }
